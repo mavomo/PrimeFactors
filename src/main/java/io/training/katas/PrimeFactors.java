6bda136e9ec29f7de21f12ed043ad32c2493e2f4
mavomo <michelle.avomo@gmail.com>
2017-01-28 11:17:06 +0100

    Handling prime factor of 4

--- src/main/java/io/training/katas/PrimeFactors.java
+++ src/main/java/io/training/katas/PrimeFactors.java
@@ -8,10 +8,14 @@ import java.util.List;
  */
 public class PrimeFactors {
 
-    public static List<Integer> generate(int i) {
+    public static List<Integer> generate(int number) {
         List<Integer> primeFactors = new ArrayList<>();
-        if(i > 1){
-            primeFactors.add(i);
+        if (number%2 == 0){
+            primeFactors.add(2);
+            number/=2;
+        }
+        if(number > 1){
+            primeFactors.add(number);
         }
         return primeFactors;
     }
