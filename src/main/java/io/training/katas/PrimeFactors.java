9415378ffe454d93e77c9b3b70cdf2f211b378bb
mavomo <michelle.avomo@gmail.com>
2017-01-28 11:03:31 +0100

    Setting up the kata with initial case

---
+++ src/main/java/io/training/katas/PrimeFactors.java
@@ -0,0 +1,13 @@
+package io.training.katas;
+
+import java.util.List;
+
+/**
+ * Created by Michelle on 28/01/2017.
+ */
+public class PrimeFactors {
+
+    public static List<Integer> generate(int i) {
+        return null;
+    }
+}
---
+++ src/test/java/io/training/katas/PrimeFactorsTest.java
@@ -0,0 +1,16 @@
+package io.training.katas;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+import java.util.List;
+
+public class PrimeFactorsTest {
+
+    @Test
+    public void shoud_return_empty_list_as_factor_of_1() {
+        List<Integer> primeFactors = PrimeFactors.generate(1);
+        Assertions.assertThat(primeFactors).isEmpty();
+
+    }
+}
