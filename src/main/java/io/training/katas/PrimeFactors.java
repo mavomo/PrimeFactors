7e7748348dcf9d5bd0be10ca1573d80d22992f0c
mavomo <michelle.avomo@gmail.com>
2017-01-28 11:48:18 +0100

    last commit

--- src/main/java/io/training/katas/PrimeFactors.java
+++ src/main/java/io/training/katas/PrimeFactors.java
@@ -3,9 +3,6 @@ package io.training.katas;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by Michelle on 28/01/2017.
- */
 public class PrimeFactors {
 
     public static List<Integer> generate(int number) {
